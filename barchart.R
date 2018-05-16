1c32541914431f7a6c005f09f8102632e01a1cc1
nimukher <39306894+nimukher@users.noreply.github.com>
2018-05-16 11:06:41 -0400

    saving barchart.R

--- barchart.R
+++ barchart.R
@@ -14,7 +14,8 @@ df <- read.csv('raw_data.csv')
 
 ####################################
 ##### write output file ############
-# add your R code to write output file
+write.csv(df, file = 'barchart.csv')
 ####################################
+
 
 
