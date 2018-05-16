e74acb571bebfffe0ff1b1d997f0cc4bbf028f4a
alexbruso <39306864+alexbruso@users.noreply.github.com>
2018-05-16 14:28:40 -0400

    saving map_of_USA.R

--- map_of_USA.R
+++ map_of_USA.R
@@ -9,6 +9,22 @@ df <- read.csv('raw_data.csv')
 ####################################
 
 ## start writing your R code from here
+library(ggmap)
+
+dummyDF <- data.frame(state.name, stringsAsFactors = FALSE)
+dummyDF$state <- tolower(dummyDF$state.name)
+us <- map_data("state")
+
+map.simple <- ggplot(dummyDF, aes(map_id = state))  
+map.simple <- map.simple +  
+  geom_map(map = us, fill="light blue", color="black") 
+
+map.simple <- map.simple +
+  expand_limits(x = us$long, y = us$lat)
+
+map.simple <- map.simple +
+  coord_map() + ggtitle("Map of USA")
+map.simple
 
 ## end your R code and logic 
 
